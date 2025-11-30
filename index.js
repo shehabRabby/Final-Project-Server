179796f5b1f38019a7cbfa7f3e02b294bea5a5f3
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2025-11-30 13:15:21 +0600

    Show available parcels for a rider

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const crypto = require("crypto");
 const admin = require("firebase-admin");
 const serviceAccount = require("./verifycationKey.json");
 const { use } = require("react");
+const { cursorTo } = require("readline");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -160,6 +161,21 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/parcels/rider", async (req, res) => {
+      const { riderEmail, deliveryStatus } = req.query;
+      const query = {};
+      if (riderEmail) {
+        query.riderEmail = riderEmail;
+      }
+      if (deliveryStatus) {
+        query.deliveryStatus = deliveryStatus;
+      }
+
+      const cursor = parcelsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.get("/parcels/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -187,7 +203,7 @@ async function run() {
           riderEmail: riderEmail,
         },
       };
-      const result = await parcelsCollection.updateOne(query,updatedDoc);
+      const result = await parcelsCollection.updateOne(query, updatedDoc);
 
       //update rider information
       const riderQuery = { _id: new ObjectId(riderId) };
